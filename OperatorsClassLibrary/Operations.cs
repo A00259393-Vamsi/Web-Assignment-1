c197969bbc5bfb756ae9ce71b7c17eac49adfa8e
Vamsi Krishna Allu <A00259393@mycambrian.ca>
2022-10-14 20:37:18 -0400

    Web-Assignment-1 Added class library for operations add subtract multiply and divide

---
+++ OperatorsClassLibrary/Operations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsClassLibrary
+{
+    public class Operations
+    {
+        public static double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double Subtract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public static double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public static double Divide(double number1, double number2)
+        {
+            double answer;
+
+            if (number2 == 0)
+            {
+                answer = -1;
+            }
+            else
+            {
+                answer = number1 / number2;
+            }
+            return answer;
+        }
+    }
+}
